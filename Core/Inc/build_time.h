11c302c472c6b11312e199ab9489b7dfcb85bd57
VoiD7965 <53878357+VoiD7965@users.noreply.github.com>
2026-06-13 13:57:56 +0200

    t

--- Core/Inc/build_time.h
+++ Core/Inc/build_time.h
@@ -2,8 +2,8 @@
 #define BUILD_TIME_H
 
 /* ===== RTC TIME ===== */
-#define BUILD_SEC      0x24
-#define BUILD_MIN      0x34
+#define BUILD_SEC      0x00
+#define BUILD_MIN      0x58
 #define BUILD_HOUR     0x13
 
 /* ===== RTC DATE ===== */
